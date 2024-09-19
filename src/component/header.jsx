05b20041ec34be514713a9a925c7c313fb972c2b
jigar makwana <jigarmakwana34065@gmail.com>
2024-09-20 03:21:03 +0530

    pe setup

---
+++ src/component/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="main">
+                            
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default header
