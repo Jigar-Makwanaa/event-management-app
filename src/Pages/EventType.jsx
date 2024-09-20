d4990e7c4f687254529a276d874cb3e9ff6ce30d
jigar makwana <jigarmakwana34065@gmail.com>
2024-09-20 19:41:39 +0530

    working on app design

--- src/Pages/EventType.jsx
+++ src/Pages/EventType.jsx
@@ -13,11 +13,44 @@ const EventType = () => {
                         <div className="main">
                             <div className="box">
                                 <div className="content">
-                                    <div className="image">
-
+                                    <div className="icon">
+                                        <img src="../today.png" alt="" />
                                     </div>
                                     <div className="title">
                                         <h3>today</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="box">
+                                <div className="content">
+                                    <div className="icon">
+                                        <img src="../new.png" alt="" />
+                                    </div>
+                                    <div className="title">
+                                        <h3>latest</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="box">
+                                <div className="content">
+                                    <div className="icon">
+                                        <img src="../upcoming.png" alt="" />
+                                    </div>
+                                    <div className="title">
+                                        <h3>upcoming</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="box">
+                                <div className="content">
+                                    <div className="icon">
+                                        <img src="../event.png" alt="" />
+                                    </div>
+                                    <div className="title">
+                                        <h3>all events</h3>
                                     </div>
                                 </div>
                             </div>
