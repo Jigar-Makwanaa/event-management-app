47a5f5115c50ab2ba1229bc306f6211b8c9e0899
jigar makwana <jigarmakwana34065@gmail.com>
2024-09-24 07:14:53 +0530

    working on ui

--- src/Pages/AddEvent.jsx
+++ src/Pages/AddEvent.jsx
@@ -4,9 +4,46 @@ import Header from '../component/Header'
 const AddEvent = () => {
     return (
         <>
-            <Header/>
+            <Header />
 
-            <h1>add event</h1>
+            {/* add-event section start  */}
+            <div className="add-event-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="main">
+                            <div className="add-event">
+                                <div className="form">
+                                    <form action="">
+                                        <div className="title">
+                                            <input type="text" placeholder='Event Title' />
+                                        </div>
+                                        <div className="description">
+                                            <input type="text" placeholder='Description' />
+                                        </div>
+                                        <div className="date">
+                                            <input type="date" />
+                                        </div>
+                                        <div className="location">
+                                            <input type="text" placeholder='Location' />
+                                        </div>
+                                        <div className="attendees">
+                                            <input type="number" placeholder='Max attendees' />
+                                        </div>
+
+                                        <div className="btn">
+                                            <button type='submit'>
+
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* add-event section end */}
         </>
     )
 }
